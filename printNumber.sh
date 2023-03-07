1b3e1cae94da6e6d67b2752dc3acec5af5c39c8a
shephali09 <hiremathshephali9@gmail.com>
2023-03-07 10:56:21 +0530

    Add: Store the data in array

--- printNumber.sh
+++ printNumber.sh
@@ -1,11 +1,18 @@
 i=1
+j=0
 while [ $i -le 10 ]
 do
   if [ $i -eq 3 ] || [ $i -eq 5 ] || [ $i -eq 6 ] || [ $i -eq 8 ]
   then
+    # skip this number
     i=$((i+1))
-    continue
+  else
+    # print this number and add to array
+    echo $i
+    arr[$j]=$i
+    j=$((j+1))
+    i=$((i+1))
   fi
-  echo $i
-  i=$((i+1))
 done
+
+echo "Array contents: ${arr[@]}"
